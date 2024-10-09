316c9b92b3fa391ee4547f22e9f27a91cd6801c9
luiss1234 <756101@student.dusd.net>
2024-10-09 14:36:28 -0700

    Conventions and Syntacs
    I added a conventions and Syntax to my cube to see its placement.

---
+++ ScriptingProjectP8Arana/Assets/Scripts/ConventionsAndSyntax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //I want to get the X location of a game object and show it on console
+        Debug.Log(transform.position.x);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // I want to know when a game object y position is lower than 5
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground");
+        }
+        
+    }
+}
