21dec8d6bc0fe44714c6e6e0377685de9ecd52c3
luiss1234 <756101@student.dusd.net>
2024-10-18 15:23:50 -0700

    If Statements
    I added if Statements to my scene about coffee being hot and cold.

---
+++ ScriptingProjectP8Arana/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+
+    void TemperatureTest()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+
+            print("Coffee is too cold.");
+        }
+
+        else
+        {
+
+            print("Coffee is just right");
+        }
+    }
+
+}
